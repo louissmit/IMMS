7f61f187a05431710642492740d0f82fa3856668
louissmit <ghr1Co74G9>
2013-11-26 18:03:50 +0100

    not working

--- assignment3/main.m
+++ assignment3/main.m
@@ -4,29 +4,35 @@ imageNames = {imageNames.name}';
 
 frame1 = rgb2gray(im2double(imread(fullfile(workingDir,imageNames{1}))));
 [m,n] = size(frame1);
-[c,r] = HarrisCornerDetector(frame1,0.7,15,0.0002);
+[c,r] = HarrisCornerDetector(frame1,0.5,15,0.0002);
 regionSize = 16;
 
 for i = 1:length(imageNames)
     frame2 = rgb2gray(im2double(imread(fullfile(workingDir,imageNames{i}))));
     imshow(frame1)
     hold on
-    [Vx, Vy] = opticalFlow(frame1, frame2, regionSize);
-
-    X = 1:regionSize:n;
-    Y = 1:regionSize:m;
-    quiver(X, Y, Vx, Vy);
-%     scatter(c,r,40,'MarkerEdgeColor','b','MarkerFaceColor','c','LineWidth',1.5)
+%     [Vx, Vy] = opticalFlow(frame1, frame2, regionSize);
+% 
+%     X = 1:regionSize:n;
+%     Y = 1:regionSize:m;
+%     quiver(X, Y, Vx, Vy);
+    scatter(c,r,40,'MarkerEdgeColor','b','MarkerFaceColor','c','LineWidth',1.5)
     hold off
-%     for j = 1:length(c)
-%         [dx, dy] = pointFlow(frame1, frame2, c(j), r(j), 16);
-%         c(j) = c(j) + dx;
-%         r(j) = r(j) + dy;
-%     end
+    new_c = c;
+    new_r = r;
+    for j = 1:length(c)
+        if(c(j) < n & r(j) < m)
+            [dx, dy] = pointFlow(frame1, frame2, c(j), r(j), 16);
+            new_c(j) = c(j) + round(dx);
+            new_r(j) = r(j) + round(dy);
+
+        end
+    end
+    c = new_c;
+    r = new_c;
     F(i) = getframe;
     frame1 = frame2;
 
 end
-% 
-% % close(outputVideo);
+
 movie(F, 5, 20);
--- assignment3/pointFlow.m
+++ assignment3/pointFlow.m
@@ -3,40 +3,36 @@ function [new_x, new_y] = pointFlow(frame1, frame2, x, y, regionSize)
 %   Detailed explanation goes here
     [m, n] = size(frame1);
     
-    gridSizeX = m / regionSize;
-    gridSizeY = n / regionSize;
-    
-    A = zeros(regionSize * regionSize, 2);
-    Vx = zeros(gridSizeX, gridSizeY);
-    Vy = zeros(gridSizeX, gridSizeY);
-    
-    
     [Gx,Gy] = imgradientxy(frame1);
     
-    h = regionSize/2 - 1
-    xshift = 0;
-    yshift = 0;
+    h = regionSize/2;
+    lh = h;
+    rh = h;
+    uh = h;
+    dh = h;
     if(x < h)
-        xshift = h;
-    elseif(x > (h - m))
-        xshift = h - m;
+        lh = x-1;
+    elseif(x > (n - h))
+        rh = x - n;
     end
     if(y < h)
-        yshift = h;
-    elseif(y > (h - m))
-        yshift = h - m;
+        dh = y-1;
+    elseif(y > (m - h))
+        uh = y - m;
     end
-    Ix = Gx((x-h):(x+h),(y-h):(y+h));
-    Iy = Gx((x-h):(x+h),(y-h):(y+h));
+    Ix = Gx((y-dh):(y+uh),(x-lh):(x+rh));
+    Iy = Gx((y-dh):(y+uh),(x-lh):(x+rh));
     It = frame2 - frame1;
+    It = It((y-dh):(y+uh),(x-lh):(x+rh));
     
+    [im, in] = size(Ix);
+    vSize = im*in;
     % calculate velocity   
-
-    A = [reshape(Ix, regionSize * regionSize, 1), reshape(Iy, regionSize * regionSize, 1)];
-    b = -reshape(It,regionSize * regionSize,1);
+    A = [reshape(Ix, vSize, 1), reshape(Iy, vSize, 1)];
+    b = -reshape(It,vSize,1);
     v = linsolve(A,b);
-    Vx(i,j) = v(1);
-    Vy(i,j) = v(2);
+    new_x = v(1);
+    new_y = v(2);
 
 end
 
