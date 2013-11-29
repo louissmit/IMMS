dd8a566700bb5e68a599847010e7d4a3864b5d57
louissmit <ghr1Co74G9>
2013-11-29 15:38:46 +0100

    messy code

--- assignment4/main.m
+++ assignment4/main.m
@@ -5,17 +5,18 @@ image2 = rgb2gray(im2double(imread('right.jpg')));
 
 [matches] = vl_ubcmatch(ldesc, rdesc);
 
-P = 20;
+P = 150;
+r = randi([1 size(matches,2)],1,P);
 N = 1;
 X = [];
 Y = [];
-PM = [];
-pY = [];
-for i = 1:P
+avgPm = zeros(6,1);
+for p = 1:P
+    i = r(p);
     index = matches(:,i);
     lframe = lframes(:,index(1));
     rframe = rframes(:,index(2));
-    x = lframe(1)
+    x = lframe(1);
     X(i) = x;
     y = lframe(2);
     Y(i) = y;
@@ -25,7 +26,24 @@ for i = 1:P
     A = [x y 0 0 1 0;
          0 0 x y 0 1];
     pm = linsolve(A,b);
+    avgPm = avgPm + pm;
 end
+V = lframes(1:2,:);
+X = V(1,:);
+Y = V(2,:);
+pX = [];
+pY = [];
+avgPm = avgPm / P;
+for i = 1:size(matches,2)
+    x = X(i);
+    y = Y(i);
+    A = [x y 0 0 1 0;
+         0 0 x y 0 1];
+    p = A * avgPm;
+    pX(i) = p(1);
+    pY(i) = p(2);
+end
+size(pX)
 imshow(image1);
 hold on;
 scatter(X,Y,40,'MarkerEdgeColor','b','MarkerFaceColor','c','LineWidth',1.5);
