426c2de1bdbe8ee148d1de0ab9b2f19f7dbc9596
louissmit <ghr1Co74G9>
2013-12-13 15:18:08 +0100

    histograms

--- project/main.m
+++ project/main.m
@@ -1,19 +1,29 @@
 workingDir = 'data/';
-locations = {'airplanes_train', 'cars_train','faces_train', 'motorbikes_train'};
-nrOfImages = 10;
+locations = {'airplanes_', 'cars_','faces_', 'motorbikes_'};
+nrOfImages = 5;
 data = [];
 for l = 1:length(locations)
-    directory = strcat(workingDir, locations{l})
+    directory = strcat(workingDir, locations{l}, 'train');
     imageNames = dir(fullfile(directory,'*.jpg'));
     imageNames = {imageNames.name};
     for i = 1:nrOfImages
         image = rgb2gray(im2double(imread(fullfile(directory,imageNames{i}))));
         [frames, desc] = vl_sift(single(image));
-        size(desc)
-        data = horzcat(data, desc);
-        
+        data = horzcat(data, desc); 
     end
 end
 
-numClusters = 400;
-[centers, assignments] = vl_ikmeans(data, numClusters);
+k = 400;
+[centers, assignments] = vl_ikmeans(data, k);
+
+for l = 1:length(locations)
+    directory = strcat(workingDir, locations{l}, 'test');
+    imageNames = dir(fullfile(directory,'*.jpg'));
+    imageNames = {imageNames.name};
+    image = rgb2gray(im2double(imread(fullfile(directory,imageNames{1}))));
+    [frames, desc] = vl_sift(single(image));
+    indices = vl_ikmeanspush(desc,centers);
+    H = vl_ikmeanshist(k,indices);
+    figure;
+    bar(H);
+end
