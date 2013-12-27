740e00357adf911f9efc85d4279f1257330d9e26
louissmit <ghr1Co74G9>
2013-12-27 15:27:02 +0100

    codebooks

--- project/main.m
+++ project/main.m
@@ -14,14 +14,14 @@ params.codeSetSize = 400;
 
 trainSetSizes = [600];%[200, 400, 800, 1600];
 params.kernel = '';
-Ks = [800, 1600];%, 2000 ,4000];
-for k = Ks
-    params.k = k;
-    runExperiment(trainSetSizes, trainingSet, testSet, params);
-end
-params.k = 400;
+% Ks = [800, 1600];%, 2000 ,4000];
+% for k = Ks
+%     params.k = k;
+%     runExperiment(trainSetSizes, trainingSet, testSet, params);
+% end
+% params.k = 400;
 
-sift_types = ['grey_scale' , 'rgb', 'opponent'];
+sift_types = ['rgb', 'opponent'];
 for sift_type = sift_types
     params.sift_type = sift_type;
     runExperiment(trainSetSizes, trainingSet, testSet, params);
